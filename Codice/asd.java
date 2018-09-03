fb002046c01e74f829ec5ebac5090981fbdbce99
Fadil Smajilbasic <37296353+FadilSmajilbasic@users.noreply.github.com>
2018-09-03 15:13:12 +0200

    Update asd.java

--- Codice/asd.java
+++ Codice/asd.java
@@ -6,7 +6,7 @@ public class asd {
     public static void main(String[] args) {
         System.out.print("asd");
         System.out.println("asd with printline");
-        
+        System.out.println("neee modific ");
         
     }
 }
