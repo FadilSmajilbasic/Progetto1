4e25df530f71826160ab3762bab5297d6cb032ee
FadilSmajilbasic <fadil.smajilbasic@samtrevano.ch>
2018-10-12 16:27:40 +0200

    latest commit

--- Codice/main.js
+++ Codice/main.js
@@ -19,50 +19,142 @@ function checkValues() {
     var surname = escapeHtml(document.getElementsByName('surname')[0].value);
     var streetNumber = escapeHtml(document.getElementsByName('streetNumber')[0].value);
 
-    var date = escapeHtml(document.getElementsByName('birthdate')[0].value);
+    var birthdate = new Date(escapeHtml(document.getElementsByName('birthdate')[0].value));
     var gender = escapeHtml(document.getElementsByName('gender')[0].value);
-    var job = escapeHtml(document.getElementsByName('job')[0].value);
     var city = escapeHtml(document.getElementsByName('city')[0].value);
     var zip = escapeHtml(document.getElementsByName('zip')[0].value);
     var phone = escapeHtml(document.getElementsByName('phone')[0].value);
+    var phonePrefix = escapeHtml(document.getElementsByName('countryCode')[0].value);
     var email = escapeHtml(document.getElementsByName('email')[0].value);
     var hobby = escapeHtml(document.getElementsByName('hobby')[0].value);
-
-    var regex = /^\d+$/;
-    console.log(regex.test(phone));
+    var job = escapeHtml(document.getElementsByName('job')[0].value);
 
 
-    if (name.length >= 2) {
-        if (address.length >= 5) {
-            if (regex.test(phone) && phone.length == 10) {
-                if (regex.test(cellphone) && cellphone.length == 10) {
-                    if (username.length >= 5) {
-                        if (password.length >= 12) {
-                            alert("validations passed");
-                            return true;
+
+
+    var regexPhone = /^\d+$/;
+    var regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var regexText = /[a-z+A-Z]/g;
+
+    var regexStreetNumber = /[a-z+A-Z+\d]/g;
+
+    // console.log(regex.test(phone));
+    console.log("test: " + regexPhone.test(phone));
+
+    if (name.length > 0 && name.length <= 50 && regexText.test(name)) {
+        if (surname.length > 0 && surname.length <= 50 && regexText.test(surname)) {
+            if (streetNumber.length > 0 && streetNumber.length <= 50 && regexStreetNumber.test(streetNumber) ) {
+                if (new Date() > birthdate) {
+                    if (gender === "M" || gender === "F" || gender === "altro") {
+                        if (city.length > 0 && city.length <= 50) {
+                            if (zip.length > 0 && zip.length <= 16) {
+                                if (regexPhone.test(phone) && phone.lenght <= 50) {
+                                    if (phonePrefix.lenght > 0) {
+                                        if (regexEmail.test(String(email).toLowerCase())) {
+                                            if (hobby.lenght <= 500) {
+                                                if (job.lenght <= 500) {
+                                                    alert("validations passed");
+                                                    return true;
+                                                } else {
+                                                    alert("job");
+                                                    
+                                                    invalidInput(document.getElementsByName("job")[0]);
+                                                }
+                                            } else {
+                                                alert("hobby");
+                                                
+                                                invalidInput(document.getElementsByName("hobby")[0]);
+                                            }
+                                        } else {
+                                            alert("email");
+                                            
+                                            invalidInput(document.getElementsByName("email")[0]);
+                                        }
+                                    } else {
+                                        alert("prefix");
+                                        
+                                        invalidInput(document.getElementsByName("prefix")[0]);
+                                    }
+
+                                } else {
+                                    alert("phone");
+                                    
+                                    invalidInput(document.getElementsByName("name")[0]);
+                                }
+                            } else {
+                                alert("zip");
+                                
+                                invalidInput(document.getElementsByName("zip")[0]);
+                            }
                         } else {
-                            alert("LA PASSWORD DEVE ESSERE LUNGA 12 CARATTERI");
+                            alert("city");
+                            
+                            invalidInput(document.getElementsByName("city")[0]);
                         }
                     } else {
-                        alert("la lunghezza del username deve essere di 5 caratteri");
+                        alert("gender");
+                        
+                        invalidInput(document.getElementsByName("gender")[0]);
+
                     }
                 } else {
-                    alert("celullare deve contenere solo numeri e deve essere lungo 10 caratteri");
+                    alert("birthdate");
+                    
+                    invalidInput(document.getElementsByName("birthdate")[0]);
                 }
-
             } else {
-                alert("phone deve contenere solo numeri e deve essere lungo 10 caratteri");
-            }
+                alert("street name");
+                invalidInput(document.getElementsByName("streetname")[0]);
 
+            }
         } else {
-            alert("address più lungo");
+            alert("surname");
+            
+            invalidInput(document.getElementsByName("surname")[0]);
+
         }
     } else {
-        alert("nome più lungo");
+        alert("name");
+        
+        invalidInput(document.getElementsByName("name")[0]);
     }
 
+
+    function invalidInput(e) {
+        e.style.color = "red";
+    }
+
+    // if (name.length >= 2) {
+    //     if (surname.length >= 5) {
+    //         if (regex.test(phone)) {
+    //             if (streetNumber <4 && streetNumber > 0) {
+    //                 if (username.length >= 5) {
+    //                     if (password.length >= 12) {
+    //                         alert("validations passed");
+    //                         return true;
+    //                     } else {
+    //                         alert("LA PASSWORD DEVE ESSERE LUNGA 12 CARATTERI");
+    //                     }
+    //                 } else {
+    //                     alert("la lunghezza del username deve essere di 5 caratteri");
+    //                 }
+    //             } else {
+    //                 alert("celullare deve contenere solo numeri e deve essere lungo 10 caratteri");
+    //             }
+
+    //         } else {
+    //             alert("phone deve contenere solo numeri e deve essere lungo 10 caratteri");
+    //         }
+
+    //     } else {
+    //         alert("address più lungo");
+    //     }
+    // } else {
+    //     alert("nome più lungo");
+    // }
+
     alert("validation failed false");
-    returnToPreviousPage();
+    // returnToPreviousPage();
     return false;
 
 
---
+++ Diario/2018_10_12_I3_Smajilbasic_Diario_Progetto1.md
@@ -0,0 +1,26 @@
+# PROGETTO | Diario di lavoro - 12.10.2018
+##### Fadil Smajilbasic
+### Canobbio, [12.10.2018]
+
+## Lavori svolti
+
+Ho continutato con l'implementazione
+Ho iniszaito a fare i vari controlli lato client 
+
+####Problemi riscontrati e soluzioni adottate
+Mi serviva un select con i prefissi del telefono di tutti i paesi
+l'ho trovato qua:
+https://gist.github.com/andyj/7108917
+
+regex per validare le email:
+https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+
+Mi serviva un popup message per i campi non validi:
+https://leaverou.github.io/bubbly/
+---
+####Punto della situazione rispetto alla pianificazione
+
+sono avanti rispetto al gantt preventivo e l'ho riportato al gantt consuntivo
+
+####Programma di massima per la prossima giornata di lavoro
